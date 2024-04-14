466a4a949204d20738b44ad4f3083f15f6bcdb65
han19githtbr <milliance23@gmail.com>
2024-04-14 18:45:50 -0300

    update

--- text_collect.py
+++ text_collect.py
@@ -1,6 +1,8 @@
-import numpy
 import cv2
 import mahotas
+import matplotlib.pyplot as plt
+import math
+import pytesseract
 
 def print_image_wait(label, img):
     cv2.imshow(label, img)
@@ -19,103 +21,69 @@ def draw_contours(img, objects):
         cv2.drawContours(img, objects,c,(0,128,0),2)
 
 
-#open target image
-target_img = cv2.imread('asserts/alpha2.png')
-#open alphabet image
-alpha_img = cv2.imread('asserts/alpha2.png')
+# Abrir imagem alvo
+target_img = cv2.imread('asserts/alpha.png')
 
+#open alphabet image
+alpha_img = cv2.imread('asserts/alpha.png')
+
+# Redimensionar imagem alvo
 target_img = cv2.resize(target_img, (0,0), fx = 2.0, fy = 2.0)
 
-#gray scale convert images
+# Converter imagem para escala de cinza
 target_img_grayscale = cv2.cvtColor(target_img, cv2.COLOR_RGB2GRAY)
 alpha_img_grayscale = cv2.cvtColor(alpha_img, cv2.COLOR_BGR2GRAY)
 
-cv2.imshow('GrayScaler', target_img_grayscale)
-cv2.waitKey(0)
+# Exibir imagem alvo em escala de cinza
+plt.imshow(target_img_grayscale, cmap='gray')
+plt.title('GrayScaler')
+plt.show()
 
-#Step 2: Bilateral filter
+# Aplicar filtro bilateral
 target_smooths = cv2.bilateralFilter(target_img_grayscale, 10, 75, 75)
 alpha_smooths = cv2.bilateralFilter(alpha_img_grayscale, 10, 75, 75)
 
-cv2.imshow('Bilateral filter', target_smooths)
-cv2.waitKey(0)
-#Step 3: Binarization resulting in white and black pixels
+# Exibir imagem suavizada com filtro bilateral
+plt.imshow(target_smooths)
+plt.title('Bilateral filter')
+plt.show()
 
+# Binarização resultando em pixels brancos e pretos
 target_bin = BinarizationImg(target_smooths)
 alpha_bin = BinarizationImg(alpha_smooths)
 
-cv2.imshow('Binarization', target_bin)
-cv2.waitKey(0)
+plt.imshow(target_bin)
+plt.title('Binarization')
+plt.show()
 
-
-#Step 4: Detect edges using Canny
+# Detectar bordas usando Canny
 target_edges = cv2.Canny(target_bin, 70, 150)
 alpha_edges = cv2.Canny(alpha_bin, 70, 150)
 
-cv2.imshow('Canny', target_edges)
-cv2.waitKey(0)
+plt.imshow(target_edges)
+plt.title('Canny')
+plt.show()
 
-
-#Step 5: Identify contours
-
+# Identificar contornos
 target_contours, target_hierarchy = cv2.findContours(target_edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 alpha_contours, alpha_hierarchy = cv2.findContours(alpha_edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
 
-cv2.imshow('Target', target_img)
+plt.imshow(target_img)
+plt.title('Target')
 
+
+# Loop sobre os contornos
 for c in target_contours:
-    x,y,w,h = cv2.boundingRect(c)
+    x, y, w, h = cv2.boundingRect(c)
     curt = target_img[y:y+h, x:x+w]
-    print_image_wait('Curt', curt)
+    # Exibir a letra recortada dentro do loop
+    plt.imshow(curt, cmap='gray')
+    plt.title('Curt')
+    plt.show(block=False)  # Defina block=False para que a execução do código continue imediatamente
+    plt.pause(0.01)  # Aguarde um curto período de tempo para verificar se a janela foi fechada
+    # Aguarde até que a janela seja fechada pelo usuário
+    while plt.fignum_exists(1):
+        plt.pause(0.1)  # Aguarde 100ms antes de verificar novamente
+    plt.close()  # Feche a janela atual antes de continuar para a próxima letra
 
-
-'''
-list_char = list()
-for c in target_contours:
-    score = 10.0
-    global cour
-    isEnter = False
-    for l, i in zip(alpha_contours, range(len(alpha_contours))):
-        ret = cv2.matchShapes(c, l, cv2.CONTOURS_MATCH_I3, 0.0)
-
-        if ret < score:
-            score = ret
-            cour = i
-            isEnter = True
-    if isEnter == True:
-        isEnter = False
-        list_char.append(cour)
-
-print(list_char)
-
-
-alphabet = list()
-
-alphabet.append([alpha_contours[25], 'A'])
-alphabet.append([alpha_contours[26], 'B'])
-alphabet.append([alpha_contours[24], 'C'])
-alphabet.append([alpha_contours[23], 'D'])
-alphabet.append([alpha_contours[22], 'E'])
-alphabet.append([alpha_contours[21], 'F'])
-alphabet.append([alpha_contours[20], 'G'])
-alphabet.append([alpha_contours[19], 'H'])
-alphabet.append([alpha_contours[18], 'I'])
-alphabet.append([alpha_contours[17], 'J'])
-alphabet.append([alpha_contours[16], 'K'])
-alphabet.append([alpha_contours[15], 'L'])
-alphabet.append([alpha_contours[14], 'M'])
-alphabet.append([alpha_contours[13], 'N'])
-alphabet.append([alpha_contours[12], 'Q'])
-alphabet.append([alpha_contours[11], 'O'])
-alphabet.append([alpha_contours[10], 'P'])
-alphabet.append([alpha_contours[9], 'R'])
-alphabet.append([alpha_contours[8], 'S'])
-alphabet.append([alpha_contours[7], 'T'])
-alphabet.append([alpha_contours[6], 'U'])
-alphabet.append([alpha_contours[5], 'Y'])
-alphabet.append([alpha_contours[4], 'V'])
-alphabet.append([alpha_contours[3], 'W'])
-alphabet.append([alpha_contours[2], 'X'])
-alphabet.append([alpha_contours[1], 'Z'])
-'''
